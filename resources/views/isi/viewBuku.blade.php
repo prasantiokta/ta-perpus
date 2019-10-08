9968203fc590019d56f9c5c84fe319cf78894a1d
indrabintang84 <indrabintang84@gmail.com>
2019-10-08 09:13:41 +0700

    datatables tambahan plugin

--- resources/views/isi/viewBuku.blade.php
+++ resources/views/isi/viewBuku.blade.php
@@ -108,10 +108,22 @@
     <br><br>
 </div>
 <!-- App ctrl angular -->
+<script src = "https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js" defer ></script>
+<script src = "https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js" defer ></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" defer ></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js" defer ></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" defer ></script>
+<script src = "https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js" defer ></script>
+<script src = "https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js" defer ></script>
 <script type="text/javascript">
 
     $(document).ready(function(){
-        $('#myTable').DataTable({});
+        $('#myTable').DataTable({
+            "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+            dom: 'Blfrtip',
+            buttons: ['excel','print'],
+            "lengthChange": true
+        });
     });
 
     var app = angular.module('tesApp', []);
