d51d30c59b211efad171a89fb531d267c2a1c635
Avi <avi.phantomhive13@gmail.com>
2019-10-28 14:10:10 +0700

    peminjaman done, insert momentjs select2

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -22,25 +22,19 @@
 	<!-- datatables -->
 	<link rel="stylesheet" href="{{ asset('addens/DataTables/datatables.min.css') }}">
 	<script type="text/javascript" src="{{ asset('addens/DataTables/datatables.min.js') }}" defer="screen"></script>
-	<!-- <script src = "https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js" defer ></script>
-	<script src = "https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js" defer ></script>
-	<script src = "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" defer ></script>
-	<script src = "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js" defer ></script>
-	<script src = "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" defer ></script>
-	<script src = "https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js" defer ></script>
-	<script src = "https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js" defer ></script> -->
-	<!-- <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" defer="screen"></script>
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
-	<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script> -->
 	<!-- fontawesome -->
 	<link rel="stylesheet" href="{{ asset('addens/fontawesome/all.min.css') }}">
 	<script type="text/javascript" src="{{ asset('addens/fontawesome/all.min.js') }}"></script>
-	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<!-- jquery growl -->
 	<link rel="stylesheet" href="{{ asset('addens/growl/jquery.growl.css') }}">
 	<script type="text/javascript" src="{{ asset('addens/growl/jquery.growl.js') }}"></script>
 	<!-- sidebar style -->
 	<link rel="stylesheet" href="{{ asset('addens/sidebar-style.css') }}">
+	<!-- select2 -->
+	<link rel="stylesheet" href="{{ asset('addens/select2/select2.min.css') }}">
+	<script type="text/javascript" src="{{ asset('addens/select2/select2.min.js') }}"></script>
+	<!-- momentjs -->
+	<script type="text/javascript" src="{{ asset('addens/momentjs/momentjs.min.js') }}"></script>
 
 
 	<!-- uwu -->
