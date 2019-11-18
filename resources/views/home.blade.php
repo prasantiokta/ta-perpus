889e48777db222ba01afd6b4de231ae3b0cfba60
Prasanti Oktaviana <avi.phantomhive13@gmail.com>
2019-11-18 23:04:39 +0700

    wes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,18 @@
 @section('page')
 <div class="container shadow-lg">
     <br><br><br>
-    <h3>Homepage</h3>
+    <center><h3>Homepage</h3></center>
     <br>
     <div class="row">
-       <div class="col-md-10 col-md-offset-1">
-           <div class="panel panel-default">
-               <div class="panel-heading"><b>Charts</b></div>
-               <div class="panel-body">
-                   <canvas id="canvas" height="280" width="600"></canvas>
-               </div>
-           </div>
+       <div class="col" style="padding: 40px;">
+        <div class="col-md-10">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Data peminjaman minggu ini</b></div>
+                <div class="panel-body">
+                    <canvas id="canvas" height="280" width="600"></canvas>
+                </div>
+            </div>
+        </div>
        </div>
     </div>
     <br><br><br>
@@ -26,11 +28,12 @@
         var total = new Array();
         $(document).ready(function(){
           $.get(url, function(response){
-            console.log(response);
-            response.forEach(function(data){
-                tgl.push(data.order_date);
+            var uwu = Object.entries(response);
+            console.log(uwu);
+            uwu.forEach(function(data){
+                tgl.push(data[0]);
                 //Labels.push(data.stockName);
-                total.push(data.total_order);
+                total.push(parseInt(data[1]));
             });
             var ctx = document.getElementById("canvas").getContext('2d');
                 var myChart = new Chart(ctx, {
